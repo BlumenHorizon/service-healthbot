3f440f4fe20a8e02df98847c7af9937bbc1f809a
Leviinson <votosi197@gmail.com>
2025-06-22 16:48:32 +0300

    chore(sqlalchemy): sqlalchemy.URL using implemented

--- src/db/database.py
+++ src/db/database.py
@@ -1,6 +1,22 @@
+from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from src.config import DATABASE_URL
+from src.config import (
+    MYSQL_DATABASE,
+    MYSQL_HOST,
+    MYSQL_PASSWORD,
+    MYSQL_PORT,
+    MYSQL_USER,
+)
+
+DATABASE_URL = URL.create(
+    "mysql+aiomysql",
+    username=MYSQL_USER,
+    password=MYSQL_PASSWORD,
+    host=MYSQL_HOST,
+    port=int(MYSQL_PORT),
+    database=MYSQL_DATABASE,
+)
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
