832378ba66f91bb482fb684da52956f2f42b29d0
Leviinson <votosi197@gmail.com>
2025-06-22 16:46:23 +0300

    chore(config): MYSQL vars added

--- src/config.py
+++ src/config.py
@@ -6,12 +6,20 @@ load_dotenv(override=True)
 
 # --- Core Configuration ---
 MODE = get_env_var("MODE", default="PROD", raise_exc=True)
-DATABASE_URL = get_env_var("DB_URL", raise_exc=True)
 BOT_TOKEN = get_env_var("BOT_TOKEN", raise_exc=True)
 
 # --- Access Control ---
 ADMIN_USERS = get_sequence_from_env("TELEGRAM_ADMINS")
 ALLOWED_CHAT_IDS = get_sequence_from_env("ALLOWED_CHAT_IDS")
+
+# --- MySQL Configuration ---
+
+MYSQL_DATABASE = get_env_var("MYSQL_DATABASE")
+MYSQL_USER = get_env_var("MYSQL_USER")
+MYSQL_PASSWORD = get_env_var("MYSQL_PASSWORD")
+MYSQL_PORT = get_env_var("MYSQL_PORT")
+MYSQL_HOST = get_env_var("MYSQL_HOST")
+
 
 # --- Redis Configration ---
 REDIS_URL = get_env_var("REDIS_URL", raise_exc=True)
